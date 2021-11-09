060dc1bcde2c4f840132d8e31e6a8ce09d9fc4f5
CinthiaElena <60494641+CinthiaElena@users.noreply.github.com>
2021-11-09 13:49:44 -0700

    Add files via upload

---
+++ proyectoP.c
@@ -0,0 +1,19 @@
+//                                    Proyecto Final Programación I
+//   Alexis Omar Garcia Talamantes, Brandon Gustavo Hernandez Ortiz, Cinthia Elena Hernandez Rodríguez 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct pelicula {
+	
+	char name[25]; 
+	char director[25];
+	int year; 
+	
+};
+
+/** --------------------------------------------- MAIN -------------------------------------------------------**/
+
+void main(){
+	
+}
